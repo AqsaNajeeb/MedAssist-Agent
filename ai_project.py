bcd5efc205de11e0edc367beff9212e7db7b41a0
Aqsa Najeeb <159306671+AqsaNajeeb@users.noreply.github.com>
2026-06-08 09:34:01 +0500

    README

--- ai_project.py
+++ ai_project.py
@@ -20,7 +20,7 @@ st.caption("AI Medical Assistant powered by Groq + PubMed + Vision AI")
 # =========================
 # SAFE GROQ SETUP (FIXED)
 # =========================
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 if not GROQ_API_KEY:
     st.error("❌ GROQ_API_KEY not found. Add it in Streamlit Secrets.")
